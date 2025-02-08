cd88516e16a6a0a29bc7920968b18455845c7ab8
TietWork <tietwork@gmail.com>
2025-02-08 09:52:43 +0530

    final

--- components/light/page-ourProcess/Header.jsx
+++ components/light/page-ourProcess/Header.jsx
@@ -26,7 +26,7 @@ function Header() {
     <div
       className="header section-padding"
       style={{
-        backgroundImage: `url('/light/assets/imgs/header/processFinal.jpg')`,
+        backgroundImage: `url('/light/assets/imgs/header/processfinal.webp')`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
