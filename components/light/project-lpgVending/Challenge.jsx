b3f64bd64b779d0fbaf29e3cfc8db8bd903dce36
TietWork <tietwork@gmail.com>
2025-02-28 11:24:46 +0530

    Update Challenge.jsx lpg
    add video and images of lpg vending news

--- components/light/project-lpgVending/Challenge.jsx
+++ components/light/project-lpgVending/Challenge.jsx
@@ -7,6 +7,101 @@ function Challenge() {
         <div className="row justify-content-center">
           <div className="col-lg-11">
             <div className="row">
+              {/* on media */}
+              <div className="col-lg-12 py-5 pt-0 position-relative">
+                <div className="orangeround"></div>
+                <div className="voiletround"></div>
+                {/* headline-on media  */}
+                <div className="col-12">
+                  <h3
+                    className="mb-30 line-height-40"
+                    style={{
+                      fontSize: "25px",
+                      fontWeight: "600",
+                      color: "#007BFF",
+                      textAlign: "left",
+                      textTransform: "uppercase",
+                    }}
+                  >
+                    On Media
+                  </h3>
+                </div>
+                {/* video  */}
+                <div className="col-12 mb-4 videolpg">
+                  <video
+                    src="/light/assets/imgs/works/lpg/lpgvideo.mp4"
+                    controls
+                    // autoPlay
+                    muted
+                    className="rounded w-100"
+                  ></video>
+                </div>
+                {/* images  */}
+                <div className="col-12 lpgvending"
+                  style={{
+                    background: 'rgba(255, 255, 255, 0.02)',
+                    border: '1px solid #FFFFFF',
+                    boxShadow: '0px 4px 16px rgba(133, 161, 255, 0.16)',
+                    backdropFilter: 'blur(8px)',
+                    borderRadius: '24px',
+                    padding: '10px',
+                  }}
+                >
+                  <div className="row" style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    padding: '0'
+                  }}>
+                    <div className="col-lg-4 col-md-6 col-sm-12 col-12 img-1">
+                      <div className="image-container">
+                        <img
+                          src="/light/assets/imgs/works/lpg/lpgimg2.jpeg"
+                          alt="Image 2"
+                        />
+                        <div className="overlay"></div>
+                      </div>
+                    </div>
+                    <div className="col-lg-4 col-md-6 col-sm-12 col-12 img-2">
+                      <div className="image-container">
+                        <img
+                          src="/light/assets/imgs/works/lpg/lpgimg1.jpeg"
+                          alt="Image 1"
+                        />
+                        <div className="overlay"></div>
+                      </div>
+                    </div>
+                    <div className="col-lg-4 col-md-12 col-sm-12 col-12 img-3">
+                      <div className="image-container">
+                        <img
+                          src="/light/assets/imgs/works/lpg/lpgimg3.jpeg"
+                          alt="Image 3"
+                        />
+                        <div className="overlay"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* content  */}
+                <div className="col-12 mt-20">
+                  <p
+                    className="fz-18"
+                    style={{
+                      color: "#000",
+                      textAlign: "left",
+                      fontFamily: "Satoshi-Variable",
+                      textAlign: 'center'
+                    }}>
+                    "We’re proud to showcase our <b style={{ fontSize: '20px' }}>Automated LPG Vending Machine</b> at India Energy Week 2025, held from February 11–14
+                    at the Yashobhoomi Convention Centre in New Delhi, revolutionizing <b style={{ fontSize: '20px' }}>LPG distribution across India.</b>"
+                    <br />
+                    <span style={{ fontSize: '17px', paddingTop: '10px' }}>
+                      "We’re also honored by PM Modi’s visit to our project at Advantage Assam 2.0 – Investment and Infrastructure Summit 2025,
+                      taking place in Assam, highlighting the national significance of this initiative."
+                    </span>
+
+                  </p>
+                </div>
+              </div>
               {/* Right Section - Project Description */}
               <div className="col-lg-12">
                 {" "}
@@ -107,7 +202,7 @@ function Challenge() {
                   </h6>
                   <p
                     className="fz-18 mb-40"
-                    style={{ color: "#000", textAlign: "left" , fontFamily: "Satoshi-Variable"}}
+                    style={{ color: "#000", textAlign: "left", fontFamily: "Satoshi-Variable" }}
                   >
                     <li>
                       <strong>• AI-Driven Decision Making:</strong> Dynamic AI
@@ -169,7 +264,7 @@ function Challenge() {
           </div>
         </div>
       </div>
-    </section>
+    </section >
   );
 }
 
